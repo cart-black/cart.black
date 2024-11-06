953d6166ff3378cd0a4bde935c9b44cb5a40e9d4
kerem <keremtiryaki@gmail.com>
2024-11-06 13:14:23 -0500

    Refactor server setup and streamline route handling in Hono application

--- apps/api-auth-hono/src/index.ts
+++ apps/api-auth-hono/src/index.ts
@@ -1,19 +1,18 @@
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
 
+const app = new Hono();
+app.use("/*", serveStatic({ root: "./static" }));
 
-const app = new Hono()
-app.use('/*', serveStatic({ root: './static' }))
+app.get("/hello", (c) => {
+	return c.text("Hello Hono!");
+});
 
-app.get('/hello', (c) => {
-  return c.text('Hello Hono!')
-})
-
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port
+});
